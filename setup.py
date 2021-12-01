3cf2b15653f4e0755a5104012b57611af8db2bbf
nitish-sharma <nisharma@redhat.com>
2021-12-01 18:37:07 +0530

    - git dependecy removed

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'decorest>=0.0.6',
         'requests',
         'requests-toolbelt>=0.9.1',
-        'pantheon-uploader @ git+https://github.com/redhataccess/pantheon-uploader.git@master#egg=pantheon-uploader-0.2'
+        'pantheon-uploader@git+https://github.com/redhataccess/pantheon-uploader.git@master#egg=pantheon-uploader-0.2'
     ],
     dependency_links=['https://github.com/redhataccess/pantheon-uploader/tarball/master#egg=pantheon-uploader'],
     entry_points=entry_points,
